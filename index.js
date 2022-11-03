147566ce3060bdd78ede2efd49468a815b66fd2a
Vishal Rikhi <rikhivva@gmail.com>
2022-11-03 15:07:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,104 @@
+const randomParagraph = document.querySelector(".random-paragraph");
+const author = document.querySelector(".author");
+const volume = document.querySelector(".volume");
+const clipboard = document.querySelector(".clipboard");
+const twitter = document.querySelector(".tweet");
+const button = document.querySelector("button");
+
+const randomParagraphGenerator = () => {
+  loading();
+  disableClicks();
+  button.innerHTML = "Loading...";
+  fetch("https://api.quotable.io/random")
+    .then((resolve) => {
+      return resolve.json();
+    })
+    .then((resolve) => {
+      randomParagraph.innerHTML = resolve.content;
+      author.innerHTML = resolve.author;
+      removeLoading();
+      enableClicks();
+      button.innerHTML = "New Quote";
+    });
+};
+
+const loading = () => {
+  button.classList.add("loading");
+};
+
+const removeLoading = () => {
+  button.classList.remove("loading");
+};
+
+const speechText = () => {
+  loading();
+  let speech = new SpeechSynthesisUtterance(
+    `${randomParagraph.innerHTML} by ${author.innerHTML}`
+  );
+  speechSynthesis.speak(speech);
+  setInterval(() => {
+    if (speechSynthesis.speaking) {
+      volume.style.background = "#5372F0";
+      volume.style.color = "white";
+    } else {
+      volume.style.background = "white";
+      volume.style.color = "#5372F0";
+      removeLoading();
+    }
+  }, 10);
+};
+
+const copyToClipBoard = () => {
+  Swal.fire({
+    title: "Do you want to copy the quote?",
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: "Yes!",
+    denyButtonText: `Don't Copy`,
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+      Swal.fire("Copied!", "", "success");
+      navigator.clipboard.writeText(randomParagraph.innerHTML);
+    } else if (result.isDenied) {
+      Swal.fire("Not Copied!", "", "info");
+    }
+  });
+};
+
+const copyTweet = () => {
+  window.open(
+    `https://twitter.com/intent/tweet?url=${randomParagraph.innerHTML}`,
+    "_blank"
+  );
+};
+
+const disableClicks = () => {
+  volume.classList.add("disable");
+  clipboard.classList.add("disable");
+  twitter.classList.add("disable");
+};
+
+const enableClicks = () => {
+  volume.classList.remove("disable");
+  clipboard.classList.remove("disable");
+  twitter.classList.remove("disable");
+};
+
+randomParagraphGenerator();
+
+button.addEventListener("click", () => {
+  randomParagraphGenerator();
+});
+
+volume.addEventListener("click", () => {
+  speechText();
+});
+
+clipboard.addEventListener("click", () => {
+  copyToClipBoard();
+});
+
+twitter.addEventListener("click", () => {
+  copyTweet();
+});
---
+++ readme.md
@@ -0,0 +1,46 @@
+## 🔗 Random-Quote-Generator-Application
+[https://vishal-expense-tracker.netlify.app/](https://vishal-expense-tracker.netlify.app/)
+
+
+## ⭐ Random-Quote-Generator-Application ⭐
+
+This is the assignment given in the **"JavaScript Full Stack Web Developer Bootcamp"** by **iNeuron** *&* This Assignment was done using **HTML5, CSS3 & Vanilla JavaScript.**
+
+
+### 📌 Tech Stack
+
+[![HTML](https://img.shields.io/badge/html5%20-%23E34F26.svg?&style=for-the-badge&logo=html5&logoColor=white)](https://github.com/pk170970)&nbsp; [![CSS](https://img.shields.io/badge/css3%20-%231572B6.svg?&style=for-the-badge&logo=css3&logoColor=white)](https://github.com/pk170970)&nbsp; ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+
+### 📌 Overview 
+
+
+![PROJECT-SCREENSHOT](./assets/vishal-random-quote-generator-application.png)
+
+
+
+### 📌 What I Learn
+
+> 👉 1. Html, Css and DOM manipulation. <br>
+  👉 2. Seprating the functionality into smaller chunks. <br>
+  👉 3. Managing Objects, Handling Event Listeners. <br>
+  👉 4. Loops and conditionals. <br>
+  👉 5. Sweet Alert Plugin. <br>
+  👉 6. Speech API. <br>
+  👉 7. fetch API. <br>
+  👉 8. Window Navigator to copy stuff into system Clipboard. <br>
+
+
+
+### Time Taken to Complete this Project:- 2 Hours
+
+### 📬 Connect With Me
+
+- **LinkedIn** - [Vishal Rikhi](https://www.linkedin.com/in/vishal-rikhi/)
+
+### 📌 Acknowledgement
+
+- **Course Instructor** - [Hitesh Choudhary](https://www.linkedin.com/in/hiteshchoudhary/)
+
+
+
